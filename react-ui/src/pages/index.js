ceee2d7d4a9e8c373b4b295b098dadaab0fa929a
Alam Ferrera <alamferrera@gmail.com>
2021-08-09 17:08:47 -0400

    footer added
    footer completed

--- react-ui/src/pages/index.js
+++ react-ui/src/pages/index.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import Footer from '../componentes/Footer';
 import HeroSection from '../componentes/HeroSection';
 import InfoSection from '../componentes/InfoSection';
 import { homeObjOne, homeObjThree, homeObjTwo } from '../componentes/InfoSection/Data';
@@ -22,6 +23,7 @@ function Home() {
             <InfoSection {...homeObjTwo} />
             <Services />
             <InfoSection {...homeObjThree} />
+            <Footer />
         </>
     )
 }
